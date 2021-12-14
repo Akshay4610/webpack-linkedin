ceeb353da5428dd670566020dba36dd900ca0693
Akshay Chandwani <akshaychandwani992@gmail.com>
2021-12-14 19:13:59 +0530

    weback configuration learning from linkedin

---
+++ src/about.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.css';
+
+import {render} from 'react-dom';
+
+const About = () => <h1>About Webpack</h1>
+
+render(<About/>, document.getElementById("target"));
---
+++ src/contact.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.css';
+
+import {render} from 'react-dom';
+
+const Contact = () => <h1>Contact Webpack</h1>
+
+render(<Contact/>, document.getElementById("target"));
